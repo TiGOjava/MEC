a04ee3415756024842fb49e3dc4abb9998a05bff
TiGOjava <jestemslim@gmail.com>
2024-01-20 20:58:25 +0100

    comit

--- src/main/java/com/MEC/Data/LocalData.java
+++ src/main/java/com/MEC/Data/LocalData.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "playlists")
+@Document(collection = "Values")
 @ToString
 @NoArgsConstructor
 @Getter
