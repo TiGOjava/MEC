cdb030498204a862950bdad97d6e049d32dad4f3
TiGOjava <jestemslim@gmail.com>
2024-01-30 06:52:25 +0100

    comitorginal

--- Data/LocalDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.MEC.Data;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface LocalDataRepository extends ReactiveMongoRepository<LocalData,String> {
-}
