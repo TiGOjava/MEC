99f05bf20a6297209c2a2f417ee446fabfa61a44
Rafal Pukalo <134229540+TiGOjava@users.noreply.github.com>
2024-03-09 05:22:07 +0100

    Update DataController.java

--- src/main/java/com/MEC/Data/DataController.java
+++ src/main/java/com/MEC/Data/DataController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-
+@Controller
 @RestController
 @RequestMapping("/api")
 public class DataController {
